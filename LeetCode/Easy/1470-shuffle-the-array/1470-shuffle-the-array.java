7a527912fbc593c665a3491337fc8dde91c7e467
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-07-03 21:34:34 +0900

    Time: 0 ms (100%), Space: 46.6 MB (41.19%) - LeetHub

---
+++ LeetCode/Easy/1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[nums.length];
+        
+        int[] arr1 = Arrays.copyOfRange(nums, 0, n);
+        int[] arr2 = Arrays.copyOfRange(nums, n, nums.length);
+        
+        int j = 0;
+        for (int i = 0; i < n; i++, j++) {
+            ans[j] = arr1[i];
+            j++;
+            ans[j] = arr2[i];
+        }
+        
+        return ans;
+    }
+}
