19c210a6e2d9e3be3f8ade141d8b6a8cce90c277
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-07-08 15:56:51 +0900

    Time: 1 ms (100%), Space: 45 MB (90.13%) - LeetHub

--- LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,23 +1,24 @@
 class Solution {
-    static Map<String, IntBinaryOperator> calculator = Map.of(
-            "+", (a, b) -> b + a,
-            "-", (a, b) -> b - a,
-            "*", (a, b) -> b * a,
-            "/", (a, b) -> b / a
-    );
-
+    int idx;
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
+        idx = tokens.length -1;
+        return help(tokens);
+    }
+    public int help(String[] s){
+        return switch(s[idx--]){
+            case "+" -> help(s) + help(s);
+            case "*" -> help(s) * help(s);
 
-        for (int i = 0; i < tokens.length; i++) {
-            String token = tokens[i];
-            IntBinaryOperator oper = calculator.get(token);
-            if (oper == null) {
-                stack.add(Integer.parseInt(token));
-            } else {
-                stack.add(oper.applyAsInt(stack.pop(), stack.pop()));
+            case "-" ->{
+                int a = help(s);
+                yield help(s) - a;
             }
-        }
-        return stack.pop();
+
+            case "/" ->{
+                int a = help(s);
+                yield help(s)/a;
+            }
+            default -> Integer.parseInt(s[idx+1]);
+        };
     }
 }
